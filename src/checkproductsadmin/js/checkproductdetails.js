275ce3c95b9b6f799e98bfb011d6f073a1e8a3af
eduar <eduarcaco@hotmail.com>
2024-12-24 15:16:54 -0600

    feat: add product management pages with styling and functionality

---
+++ src/checkproductsadmin/js/checkproductdetails.js
@@ -0,0 +1,100 @@
+var productData = JSON.parse(sessionStorage.getItem('productData'));
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    fillProductData();
+    changeButton(productData.productStatus);
+
+})
+
+function changeButton(status){
+    if(!status){
+        const button = document.getElementById("changeStatus");
+        button.classList.remove("btn-danger");
+        button.classList.add("btn-success");
+        button.textContent = "Activar";
+    } else {
+        const button = document.getElementById("changeStatus");
+        button.classList.remove("btn-success");
+        button.classList.add("btn-danger");
+        button.textContent = "Desactivar";
+    }
+}
+
+function fillProductData(){
+    document.getElementById("name").textContent = productData.name;
+    document.getElementById("categorie").textContent = productData.category.name;
+    document.getElementById("price").textContent = "$"+productData.unitPrice;
+    document.getElementById("description").textContent = productData.description;
+    document.getElementById("code").textContent = productData.barCode;
+
+    const date = convertDate(productData.expireDate);
+    const unitMeasure = convertUnitMeasure(productData.unitMeasure);
+
+    document.getElementById("expiration").textContent = date;
+    document.getElementById("weight").textContent = productData.weight + " " + unitMeasure.abreviatura;
+    document.getElementById("measurement").textContent = unitMeasure.unidad;
+
+    const imgElement = document.querySelector('.col-md-6 img');
+    imgElement.src = productData.image;
+}
+
+function convertDate(date){
+    const fecha = new Date(date);
+    const dia = fecha.getDate().toString().padStart(2, '0');
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const anio = fecha.getFullYear();
+    return `${dia}/${mes}/${anio}`;
+}
+
+function convertUnitMeasure(unitMeasure){
+    switch(unitMeasure){
+        case 'Piece':
+            return {unidad:'Piezas', abreviatura:'Pz'};
+
+        case 'Kilogram':
+            return {unidad:'Kilogramos', abreviatura:'Kg'};
+
+        case 'Gram':
+            return {unidad:'Gramos', abreviatura:'gr'};
+
+        case 'Liter':
+            return {unidad:'Litros', abreviatura:'L'};
+        
+        case 'Milliliter':
+            return {unidad:'Mililitros', abreviatura:'ml'};
+    
+        case 'Meter':
+            return {unidad:'Metros', abreviatura:'m'};
+    
+        case 'Centimeter':
+            return {unidad:'Centímetros', abreviatura:'cm'};
+    
+        case 'Inch':
+            return {unidad:'Pulgadas', abreviatura:'in'};
+
+        case 'Pack':
+            return {unidad:'Paquetes', abreviatura:'Pq'};
+        
+        case 'Box':
+            return {unidad:'Cajas', abreviatura:'Cj'};
+
+        default:
+            return unitMeasure;
+    }
+}
+
+async function changeProductStatus(){
+    try{ 
+        const status =  {newStatus: !productData.productStatus};
+        await axios.patch(`${API_URL}/products/${productData._id}`, status);
+        showToast("Estado del producto cambiado exitosamente", toastTypes.SUCCESS);
+        changeButton(!productData.productStatus);
+    }catch (error){
+        alert(error);
+        showToast(
+            "Ocurrió algo inesperado al cambiar el estado del producto. Verifique su conexión e inténtelo más tarde.",
+            toastTypes.DANGER
+        );
+    }
+}
---
+++ src/checkproductsadmin/js/checkproductsadmin.js
@@ -0,0 +1,126 @@
+// Contenedor de productos
+const productContainer = document.getElementById("product-container");
+let USER_ID
+let allProducts = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const products = await getProducts()
+    renderProducts(products);
+    await loadCategories();
+})
+
+async function getProducts() {
+    try {
+        const response = await axios.get(`${API_URL}/products`);
+        allProducts = response.data.products.map(product => ({
+            ...product,
+            simpleName: removeAccents(product.name)
+        }));
+        return response.data.products;
+    } catch (error) {
+        showToast("No se ha podido obtener ningun producto", toastTypes.WARNING);
+    }
+} 
+
+async function loadCategories() {
+    const categoriesDropdown = document.getElementById('categories');
+    categoriesDropdown.innerHTML = '<option value="" selected>Todas las categorías</option>';
+
+    try {
+        const response = await axios.get(API_URL + 'categories/', {
+            params: {
+                api_key: "00000"
+            },
+        });
+
+        response.data.category.forEach((category) => {
+            const option = document.createElement('option');
+            option.value = category._id; 
+            option.textContent = category.name; 
+            categoriesDropdown.appendChild(option); 
+        });
+    } catch (error) {
+        showToast(
+            "Ocurrió algo inesperado al cargar las categorías. Verifique su conexión e inténtelo más tarde.",
+            toastTypes.DANGER
+        );
+    }
+
+    categoriesDropdown.addEventListener("change", onCategoryChange);
+}
+
+function onCategoryChange(event) {
+    // Obtener el valor de la opción seleccionada
+    const selectedCategoryId = event.target.value;
+    
+    if (selectedCategoryId) {
+        showCategorieProducts(selectedCategoryId);
+    } else {
+        renderProducts(allProducts);
+    }
+}
+
+// Generar tarjetas dinámicamente
+function renderProducts(products) {
+    productContainer.innerHTML = "";
+    products.forEach((product, index) => {
+        const card = document.createElement("div");
+        card.className = "col-12 col-sm-6 col-md-4 col-lg-3";
+    
+        card.innerHTML = `
+            <div class="card shadow-sm">
+                <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                <div class="card-body">
+                    <h5 class="card-title text-primary">$${product.unitPrice.toFixed(2)}</h5>
+                    <p class="card-text">${product.name}</p>
+                    <div class="d-flex justify-content-between">
+                        <button class="btn btn-outline-primary" onclick="viewProduct('${product._id}')">Ver detalles</button>
+                        <button class="btn btn-primary" onclick="editProductt('${product._id}')">Editar</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        productContainer.appendChild(card);
+    });
+}
+
+function showCategorieProducts(selectedCategoryId) {
+    const products = allProducts.filter(product => product.category._id === selectedCategoryId);
+    renderProducts(products);
+}
+
+function addNewProduct(){
+    window.location.href = "http://127.0.0.1:5500/src/products/productsForm.html";
+}
+
+function viewProduct(productId) {
+    sessionStorage.clear('productData');
+    sessionStorage.setItem('productData', JSON.stringify(allProducts.find(product => product._id === productId)));
+    window.location.href = "http://127.0.0.1:5500/src/checkproductsadmin/checkproductdetails.html";
+}
+
+function searchProduct(){
+    const searchInput = removeAccents(document.getElementById("searchBar").value.toLowerCase()); 
+    let productSearched = [];
+    allProducts.forEach(product => {
+        if (product.simpleName.includes(searchInput)) {
+            productSearched.push(product); 
+        } 
+    });
+
+    if(productSearched.length === 0){
+        showToast("No se encontraron productos", toastTypes.WARNING);
+    } else {
+        renderProducts(productSearched);
+    }
+}
+
+function removeAccents(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(); 
+}
+
+function editProductt(index) {
+    // Guardar el producto en el sessionStorage
+    //Recibir el producto en el otro archivo
+    //enviar al usuario a la otra pagina
+}
