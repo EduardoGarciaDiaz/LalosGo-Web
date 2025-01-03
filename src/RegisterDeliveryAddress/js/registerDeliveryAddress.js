9f858f193d78d6ec78db5970af043d3b7a3a8a3f
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2025-01-02 22:44:19 -0600

    Refactor delivery address registration logic and improve HTML structure

--- src/RegisterDeliveryAddress/js/registerDeliveryAddress.js
+++ src/RegisterDeliveryAddress/js/registerDeliveryAddress.js
@@ -29,8 +29,6 @@ function initMap() {
 
     geocoder = new google.maps.Geocoder();
 
-
-    //Si la acción es editar o mostrar dirección, se llenan los campos con la información de la dirección
     if (ACTION_TYPE === 'EditDeliveryAddress' || ACTION_TYPE === 'ShowDeliveryAddress') {
         const customLocation = new google.maps.LatLng(initialLocation.lat, initialLocation.lng);
         getAddressFromCoordinates(customLocation);
@@ -38,7 +36,6 @@ function initMap() {
         document.getElementById("interior_number").value = deliveryAddressData.internalNumber;
     }
 
-    //Si la acción es mostrar dirección, se deshabilita la acción de seleccionar en el mapa
     if (ACTION_TYPE !== 'ShowDeliveryAddress') {
         map.addListener("click", (event) => {
             const clickedLocation = event.latLng;
@@ -129,16 +126,18 @@ async function registerDeliveryAddress(event) {
 
         if (ACTION_TYPE === 'RegisterDeliveryAddress') {
             newDeliveryAddress.isCurrentAddress = false;
-            let succes = await registerNewDeliveryAddress(newDeliveryAddress);
+            await registerNewDeliveryAddress(newDeliveryAddress);
 
         } else if (ACTION_TYPE === 'EditDeliveryAddress') {
             editDeliveryAddress(newDeliveryAddress);
 
         } else if (ACTION_TYPE === 'CreateClientAccount') {
             newDeliveryAddress.isCurrentAddress = true;
-            let succes = await registerClientAccount(newDeliveryAddress)
-            if (succes) {
-                window.location.href = "/src/login/login.html"
+            let success = await registerClientAccount(newDeliveryAddress)
+            if (success) {
+                setTimeout(() => {
+                    window.location.href = "/src/login/login.html";
+                }, 2000);
             }
         }
     }
